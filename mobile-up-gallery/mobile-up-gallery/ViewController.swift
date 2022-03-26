0994754ccfa33022e3ba6e1510de00ff0c7c9276
foggydawning <foggydawning@gmail.com>
2022-03-26 12:42:44 +0300

    create xcode project

---
+++ mobile-up-gallery/mobile-up-gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile-up-gallery
+//
+//  Created by Илья Чуб on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
