b61180072a0963134589016d39f7adc98bfb2a29
foggydawning <foggydawning@gmail.com>
2022-03-27 11:39:09 +0300

    change project structure

--- mobile-up-gallery/mobile-up-gallery/UI/MainUINavigationController.swift
+++ mobile-up-gallery/mobile-up-gallery/UI/MainUINavigationController.swift
@@ -1,19 +1,15 @@
 //
-//  ViewController.swift
+//  MainUINavigationController.swift
 //  mobile-up-gallery
 //
 //  Created by Илья Чуб on 26.03.2022.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainUINavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
