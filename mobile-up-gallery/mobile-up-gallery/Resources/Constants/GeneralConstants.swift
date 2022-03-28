a6bd838ece33c8ba6670bdead6c7a742132ec11a
foggydawning <foggydawning@gmail.com>
2022-03-28 13:00:52 +0300

    add Resources: general constants and buttons labels localizable strings

---
+++ mobile-up-gallery/mobile-up-gallery/Resources/Constants/GeneralConstants.swift
@@ -0,0 +1,12 @@
+//
+//  GeneralConstants.swift
+//  mobile-up-gallery
+//
+//  Created by Илья Чуб on 28.03.2022.
+//
+
+struct GeneralConstants {
+    enum Text {
+        static let appName = "Mobile Up Gallery"
+    }
+}
